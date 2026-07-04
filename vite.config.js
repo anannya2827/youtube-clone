96ef472fb4f1c33021dbc46f0c933256a45a6383
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2026-07-04 15:38:42 +0530

    Fix base path quotes in vite.config.js
    Ensure the base path in vite.config.js starts and ends with a single quote mark.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add the base property below if deploying to GitHub Pages:
-  base:'/youtube-clone/',
+  // Ensure the base path starts and ends with a single quote mark
+  base: '/youtube-clone/', 
 })
